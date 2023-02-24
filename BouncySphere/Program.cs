17c135e840b7bb336e59e31871a0bbb9995f8bec
thorhalbert <thorhalbert@gmail.com>
2023-02-23 20:19:01 -0800

    Emissary is starting to work.  I can round-trip wasm now.  Started with wapc library to make imports happen.

---
+++ BouncySphere/Program.cs
@@ -0,0 +1,16 @@
+﻿using BouncySphere;
+using EntityABI;
+using System.Runtime.InteropServices;
+
+Console.WriteLine($"Hello, world at {DateTime.Now.ToLongTimeString()} on {RuntimeInformation.OSArchitecture}!");
+
+//var entity = new Entity();
+//var message = entity.EnterStartup();
+
+//Console.WriteLine($"Message from ABI: {message.Name}");
+
+
+var wapc = new Wapc();
+//wapc.RegisterFunction("hello", hello);
+
+Wapc.testCall();
